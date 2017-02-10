f78abd8b133172bb0125c2bfa78313b2ab20020e
lizhaoguang <lizhaoguang@dangdang.com>
2017-02-10 20:04:16 +0800

    增加路由示例

--- app/routes.php
+++ app/routes.php
@@ -3,6 +3,10 @@
  * 路由配置文件
  * 每一项对应一个路由
  */
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
 return [
     /**
      * 一个路由配置由三项组成：
@@ -12,4 +16,8 @@ return [
      */
     ['GET', '/', ['App\Controllers\HomeController', 'hello']],
     ['GET', '/user/{id:\d+}', ['App\Controllers\HomeController', 'getUser']],
+    ['GET', '/fun', function(Request $request, Response $response) {
+        $response->getBody()->write("hello world!");
+        return $response;
+    }]
 ];
